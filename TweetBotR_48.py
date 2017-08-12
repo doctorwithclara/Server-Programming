2d972412756d1a3e6c37554b5eac43d5fb9b5c37
John Oswald <doctorwithclara@outlook.com>
2017-08-12 13:20:17 +0900

    Update TweetBotR_48.py

--- TweetBotR_48.py
+++ TweetBotR_48.py
@@ -1,10 +1,10 @@
 import tweepy
 import time, sys
 
-consumer_key = 'iiRI3RZhVYRKRpGbe91ZmXgy8'
-consumer_secret = 'hR40sQ7OqJ0Oq9dkCj0uhkp1sp21Ndvn5t9i4gWftKlbVeYB8K'
-access_token = '605285251-P1F5efNk95CnHYHxyFcVLsABCW3cG3pR64jsRNZk'
-access_token_secret= 'Qx7qTKejt5RMTbI1oajYxEQELFRczWXFgVkImiWpsqNwQ'
+consumer_key = 'iiRI3RZhVYRKRpGbe91Zm****'
+consumer_secret = 'hR40sQ7OqJ0Oq9dkCj0uhkp1sp21Ndvn5t9i4gWftKlbVe****'
+access_token = '605285251-P1F5efNk95CnHYHxyFcVLsABCW3cG3pR64js****'
+access_token_secret= 'Qx7qTKejt5RMTbI1oajYxEQELFRczWXFgVkImiWps****'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
