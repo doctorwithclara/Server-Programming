2124df125f2fbf84cff2628c39a45df5d6f3830d
John Oswald <doctorwithclara@outlook.com>
2017-08-12 13:22:55 +0900

    Update TweetBotS_48.py

--- TweetBotS_48.py
+++ TweetBotS_48.py
@@ -1,7 +1,7 @@
 #from bs4 import BeautifulSoup
 from twitter import *
 #import schedule, time, sys
-t=Twitter(auth=OAuth('605285251-P1F5efNk95CnHYHxyFcVLsABCW3cG3pR64jsRNZk','Qx7qTKejt5RMTbI1oajYxEQELFRczWXFgVkImiWpsqNwQ','iiRI3RZhVYRKRpGbe91ZmXgy8',
-'hR40sQ7OqJ0Oq9dkCj0uhkp1sp21Ndvn5t9i4gWftKlbVeYB8K'))
+t=Twitter(auth=OAuth('605285251-P1F5efNk95CnHYHxyFcVLsABCW3cG3pR64js****','Qx7qTKejt5RMTbI1oajYxEQELFRczWXFgVkImiWps****','iiRI3RZhVYRKRpGbe91Zm****',
+'hR40sQ7OqJ0Oq9dkCj0uhkp1sp21Ndvn5t9i4gWftKlbVe****'))
 tweets="Hi"
 t.statuses.update(status=tweets)
